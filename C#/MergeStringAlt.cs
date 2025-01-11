5e32bb076eb92a2373453bdf93e96edafc5a0258
Anna Free <52797630+AnnaFree@users.noreply.github.com>
2025-01-10 20:10:45 -0500

    Add files via upload

---
+++ C#/MergeStringAlt.cs
@@ -0,0 +1,38 @@
+﻿
+namespace LeetCode75Solutions
+{
+    internal class MergeStringAlt
+    {
+        string mergeAlternately(string word1, string word2)
+        {
+            string merged = "";
+
+            int i = 0, j = 0;
+
+
+            while (i < word1.Length && j < word2.Length)
+            {
+                merged += word1[i++];
+                merged += word2[j++];
+            }
+            while (i < word1.Length)
+            {
+                merged += word1[i++];
+            }
+            while (j < word2.Length)
+            {
+                merged += word2[j++];
+            }
+
+            return merged;
+        }
+        static void Main(string[] args)
+        {
+            string word1 = Console.ReadLine();
+            string word2 = Console.ReadLine();
+            MergeStringAlt program1 = new MergeStringAlt();
+            Console.WriteLine(program1.mergeAlternately(word1, word2));
+
+        }
+    }
+}
