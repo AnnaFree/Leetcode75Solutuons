97e8d97b9500a8658bff8b2379fb09e6d97c1970
Anna Free <52797630+AnnaFree@users.noreply.github.com>
2025-01-10 22:18:05 -0500

    Add files via upload

---
+++ C#/CandyArray.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LeetCode75Solutions
+{
+    internal class CandyArray
+    {
+        public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+            List<bool> result = new List<bool>();
+            int moreCandyFlag = 0;
+            for(int i = 0; i < candies.Length; i++)
+            {
+                
+                for(int j = 0; j < candies.Length; j++)
+                {
+                    if ((candies[i]+extraCandies) >= candies[j])
+                    {
+                        moreCandyFlag++;
+                    }
+                    
+                }
+                if (moreCandyFlag == candies.Length)
+                {
+                    result.Add(true);
+                }
+                else
+                {
+                    result.Add(false);
+                }
+                moreCandyFlag = 0;
+            }
+            return result;
+        }
+        public void Main(string[] args) {
+           
+        }
+    }
+}
